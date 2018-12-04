5d066023ebc6e25bc616d7986b6c5b7ca6477d53
claire-mielcarek <claire.mielcarek@lilo.org>
2018-12-03 21:17:54 -0500

    It's pretty good

--- app/src/main/java/com/example/clair/computedebits/ItemAdapter.java
+++ app/src/main/java/com/example/clair/computedebits/ItemAdapter.java
@@ -1,33 +1,48 @@
 package com.example.clair.computedebits;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
-
 import java.util.ArrayList;
 
-public class ItemAdapter extends BaseAdapter {
-    private ArrayList<Integer> datas;
-    private Context context;
 
-    public ItemAdapter(Context context, ArrayList<Integer> datas) {
+public class ItemAdapter extends BaseAdapter {
+    private ArrayList<ArrayList<String>> datas;
+    private Context context;
+    private int LINE = 0;
+    private int QTOT = 1;
+    private int UP_ELEVATION = 2;
+
+    public ItemAdapter(Context context, ArrayList<ArrayList<String>> datas) {
         this.context = context;
         this.datas = datas;
     }
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        int lineIndice = datas.get(i);
 
-        if (view ==null){
+        String lineText = datas.get(LINE).get(i);
+        String qtotText = datas.get(QTOT).get(i);
+        String upElevationText = datas.get(UP_ELEVATION).get(i);
+        Log.d("[ LIST_ADAPTER ]", "getView");
+
+        if (view == null){
             view = LayoutInflater.from(context).inflate(R.layout.see_item, viewGroup, false);
+            Log.d("[ LIST_ADAPTER ]", "View created");
         }
 
-        TextView line = (TextView) view.findViewById(R.id.line_number);
-        line.setText(""+lineIndice);
+        TextView lineView = view.findViewById(R.id.itemLineNumber);
+        TextView qtotView = view.findViewById(R.id.itemQtot);
+        TextView upElevationView = view.findViewById(R.id.itemUpElevation);
+
+        lineView.setText(""+lineText);
+        qtotView.setText(""+qtotText);
+        upElevationView.setText(""+upElevationText);
+
         return view;
     }
 
@@ -43,7 +58,7 @@ public class ItemAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
-        return datas.size();
+        return datas.get(0).size();
     }
 }
 
--- app/src/main/java/com/example/clair/computedebits/SeeActivity.java
+++ app/src/main/java/com/example/clair/computedebits/SeeActivity.java
@@ -9,8 +9,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.example.clair.computedebits.debitDevider.PowerPlant;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -22,7 +26,15 @@ import java.util.StringTokenizer;
 
 public class SeeActivity extends AppCompatActivity {
     ListView list;
-    ArrayList<Integer> qTots;
+    ArrayList<String> qTots;
+    ArrayList<String> lineNumbers;
+    ArrayList<String> upElevations;
+    ArrayList<ArrayList<Integer>> maxDebits;
+    ArrayList<ArrayList<Integer>> repartitionDebits;
+    ArrayList<ArrayList<Integer>> repartitionPowers;
+
+    int DEBIT_MAX_DEFAULT = 160;
+    int DEBIT_UNUSED_TURBINE = 0;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -33,45 +45,99 @@ public class SeeActivity extends AppCompatActivity {
         actionBar.setTitle(R.string.see_title);
 
         list = findViewById(R.id.see_list);
+        qTots = new ArrayList<>();
+        lineNumbers = new ArrayList<>();
+        upElevations = new ArrayList<>();
+        maxDebits = new ArrayList<>();
+        repartitionPowers = new ArrayList<>();
+        repartitionDebits = new ArrayList<>();
+
         readAndInsert();
+
+        ArrayList<ArrayList<String>> args = new ArrayList<>();
+        args.add(lineNumbers);
+        args.add(qTots);
+        args.add(upElevations);
+        list.setAdapter(new ItemAdapter(this, args));
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                PowerPlant powerPlant = new PowerPlant(
+                        Integer.parseInt(qTots.get(i)),
+                        Integer.parseInt(upElevations.get(i)),
+                        maxDebits.get(i));
+                Intent intent = new Intent(SeeActivity.this, ResultActivity.class);
+                ArrayList<ArrayList<Integer>> powerPlantRepartition = powerPlant.getRepartitionOpti();
+                int power = 0;
+                for (int ind = 0; ind < 5; ind++) {
+                    power += powerPlantRepartition.get(1).get(ind);
+                }
+                intent.putExtra("debits", powerPlantRepartition.get(0));
+                intent.putExtra("power", power);
+
+                startActivity(intent);
+            }
+        });
+
     }
 
     private void readAndInsert(){
-        qTots= new ArrayList<>();
         AssetManager assetManager = getAssets();
         InputStream is = null;
 
         try {
-            /*
-            for (String s : assetManager.list("")){
-                Log.d("[ READ_CSV ]", s);
-            }
-            */
-            //Log.d("[ READ_CSV ]", "assets : " + assetManager.list("").toString());
             is = assetManager.open("CCD_Data.csv");
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
         if (is != null){
-            //Log.d("[ READ_CSV ]", "is : " + is.toString());
             BufferedReader reader = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
 
-            String line = "";
-            StringTokenizer st = null;
+            String line;
+            ArrayList<Integer> currentRepartitionDebits;
+            ArrayList<Integer> currentRepartitionPowers;
+            ArrayList<Integer> currentMaxDebits;
+            int currentDebit;
+            StringTokenizer st;
+            int lineIndice = 1;
             try {
+                line = reader.readLine();
+                while (line != null) {
 
-                while ((line = reader.readLine()) != null) {
-                    st = new StringTokenizer(line, ";");
-                    //Log.d("[ READ_CSV ]", st.toString());
-                    qTots.add(Integer.parseInt(st.nextToken()));
+                    currentMaxDebits = new ArrayList<>();
+                    currentRepartitionDebits = new ArrayList<>();
+                    currentRepartitionPowers = new ArrayList<>();
+
+                    st = new StringTokenizer(line,";");
+                    lineNumbers.add(""+lineIndice);
+                    qTots.add(st.nextToken());
+                    upElevations.add(st.nextToken());
+
+                    for(int i=0; i<5; i++){
+                        currentDebit = Integer.parseInt(st.nextToken());
+                        if(currentDebit == 0) {
+                            currentMaxDebits.add(DEBIT_UNUSED_TURBINE);
+                        }
+                        else if(currentDebit < DEBIT_MAX_DEFAULT){
+                            currentMaxDebits.add(((int) (currentDebit/5) + 1) * 5);
+                        }
+                        else{
+                            currentMaxDebits.add(DEBIT_MAX_DEFAULT);
+                        }
+                        currentRepartitionDebits.add(currentDebit);
+                        currentRepartitionPowers.add(Integer.parseInt(st.nextToken()));
+                    }
+                    repartitionDebits.add(currentRepartitionDebits);
+                    repartitionPowers.add(currentRepartitionPowers);
+                    maxDebits.add(currentMaxDebits);
+                    //Log.d("[ LINE_PRINT ]", ""+lineIndice + " " + qTots.get(lineIndice-1) + " " + upElevations.get(lineIndice-1));
+                    lineIndice++;
+                    line = reader.readLine();
                 }
             } catch (IOException e) {
-
                 e.printStackTrace();
             }
         }
-        list.setAdapter(new ItemAdapter(this, qTots));
     }
 
     @Override
